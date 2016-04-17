1f1bc07b530afc4a5f1859ae1a52f3f4e060cd82
arisa kosaka <arisa-kosaka@united.jp>
2016-04-17 12:17:35 +0000

    twitter2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+
   def show
     @user = User.find(params[:id])   
   end
@@ -18,13 +19,24 @@ class UsersController < ApplicationController
     end
   end
 
- 
+ def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :area, :email, :password,
                                  :password_confirmation)
   end
   
