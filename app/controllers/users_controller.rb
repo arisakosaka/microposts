8ceaa66a407e64a2a5805d11c471c9b62793b5a0
arisa kosaka <arisa-kosaka@united.jp>
2016-04-18 08:42:41 +0000

    following2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,74 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]#これ追加！
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
+    before_action :correct_user,   only: [:edit, :update]
    
-  def show # 追加
-  @user = User.find(params[:id])
-  @microposts = @user.microposts.order(created_at: :desc)
- end
- 
- def new
-   @user = User.new
- end
- 
- def create
-   @user = User.new(user_params)
-   if @user.save
-     flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
-   else
-     render 'new'
-   end
- end
- 
-def edit
-   @user = User.find(params[:id])
- end
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
+    @following_users = @user.following_users.all
+    @follower_users = @user.follower_users.all    
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+ def edit
+    @user = User.find(params[:id])
+  end
 
- def update
-   @user = User.find(params[:id])
-   if @user.update(user_params)
-  redirect_to @user , notice: 'プロフィールを編集しました'
-   else
-     render 'edit'
-   end
- end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
 
- 
- private
+  def followings
+    @title = "Following"
+    @user = User.find(params[:id])
+    @following_users = @user.following_users.all
+  end
 
- def user_params
-   params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
- end
- 
- # beforeフィルター
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @follower_users = @user.follower_users.all
+  end
+  
+  private
 
-   # ログイン済みユーザーかどうか確認
-   def logged_in_user
-     unless logged_in?
-       flash[:danger] = "Please log in."
-       redirect_to login_url
-     end
-   end
+  def user_params
+    params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
+  end
+  
+  # beforeフィルター
 
-   # 正しいユーザーかどうか確認
-   def correct_user
-     @user = User.find(params[:id])
-     redirect_to(root_url) unless current_user?(@user)
-   end
- 
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+  
 end
