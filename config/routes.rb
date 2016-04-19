dc1fbb6d02b7fda306aefa4812f264e9826f87d3
arisa kosaka <arisa-kosaka@united.jp>
2016-04-19 00:53:12 +0000

    final4

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,9 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get    'setting' , to: 'sessions#edit'
-  get    'user/:id/followers' ,to:'users#followers', as: 'followers'
-  get    'user/:id/followings' ,to:'users#followings', as: 'followings'
-  
+
   resources :users
-  resources :sessions, only: [:new, :create, :destroy, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
   resources :relationships, only: [:create, :destroy]
-  resources :following_users
-  resources :follower_users
 end
